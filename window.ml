79d341baa4f70001ad175c15667a04290ba21287
ericlippert <eric@lippert.com>
2016-01-02 12:07:44 -0800

    More experiments in text rendering
    I've built a maquette of the interpreter that just knows how to print
    strings and ask for text, in a functional style. The interpreter knows
    whether the recent prints are going to overflow the screen. If the
    interpreter decides that it needs to prompt for text or for "more" then
    it returns halfway through an instruction and requires the caller to
    "step with input" before it continues.

--- window.ml
+++ window.ml
@@ -2,17 +2,22 @@ open Graphics;;
 
 open_graph "";;
 
+
+
 let events = [Key_pressed];;
 
 let string_of_char x = String.make 1 x;;
 
+let text_max_height = 10;;
+let text_max_width = 40;;
+
 (* Takes a list of strings, the (x, y) coordinates of the bottom left
-   corner of a window, the height of the window, and the number of pixels
+   corner of a window, the height of the window in lines, and the number of pixels
    per line. Pops the list until the window is full or the list is empty. *)
    
-let display_lines lines x y window_height line_height = 
+let display_lines lines x y max_lines line_height = 
     let rec aux lines n = 
-        if n * line_height > window_height then ()
+        if n > max_lines then ()
         else
             match lines with
             | [] -> ()
@@ -32,53 +37,199 @@ let rec reverse_index_from text target index =
     else reverse_index_from text target (index - 1);;
 
 (* Word-wraps the last line in a list of lines. Assumes that
-the tail of the list is already word-wrapped. *)
+the tail of the list is already word-wrapped. Returns the 
+new list and the number of lines added to the given list. *)
     
 let rec wrap_lines lines wrap_size =
-    match lines with
-    | [] -> []
-    | h :: t -> 
-        let len = String.length h in
-        if len > wrap_size then
-            let space_location = reverse_index_from h ' ' wrap_size in
-            let break_point = match space_location with
-                | None -> wrap_size
-                | Some location -> location in
-            wrap_lines ((String.sub h (break_point + 1) (len - break_point - 1)) :: (String.sub h 0 break_point) :: t) wrap_size
-        else
-            lines;;
+    let rec aux lines count =
+        match lines with
+        | [] -> ([], 0)
+        | h :: t -> 
+            let len = String.length h in
+            if len > wrap_size then
+                let space_location = reverse_index_from h ' ' wrap_size in
+                let break_point = match space_location with
+                    | None -> wrap_size
+                    | Some location -> location in
+                aux ((String.sub h (break_point + 1) (len - break_point - 1)) :: (String.sub h 0 break_point) :: t) (count + 1)
+            else
+                (lines, count) in
+    aux lines 0;;
             
 let add_to_lines lines str =
     match lines with 
     | [] -> [str]
     | h :: t -> (h ^ str) :: t ;;
-          
-let get_input draw max =
+    
+    
+type instruction = 
+    | Print of string
+    | Newline
+    | Prompt 
+    | Quit;;
+    
+type interpreter_state =
+    | Running
+    | Halted
+    | Printing_is_waiting_for_key
+    | Prompting_is_waiting_for_string ;;
+    
+type interpreter =
+{
+    instructions : instruction list;
+    state : interpreter_state;
+    transcript : string list;
+    lines_added_since_last_input : int;
+    pending_lines : string list
+};;
+
+(* Pops up to n items from source, pushes onto destination,
+returns popped source, pushed destination, and number of items 
+actually moved. *)
+
+let move source destination number =
+    let rec aux s d n c = 
+        if n = 0 then (s, d, c)
+        else match s with
+        | [] -> (s, d, c)
+        | h :: t -> aux t (h :: d) (n - 1) (c + 1) in
+    aux source destination number 0;;
+    
+let move_all source destination =
+    let rec aux s d c = 
+        match s with
+        | [] -> (d, c)
+        | h :: t -> aux t (h :: d) (c + 1) in
+    aux source destination 0;;
+    
+
+
+let next_instruction interpreter =
+    { interpreter with instructions = List.tl interpreter.instructions; state = Running };;
+
+let try_to_complete_printing interpreter =
+
+    (* If there are pending lines then we must have just gotten a keypress. 
+    Move them into the transcript and note how many lines were added since the keypress. *)
+    
+    let (transcript_with_pending, moved_into_transcript) = move_all interpreter.pending_lines interpreter.transcript in
+    let new_interpreter = if moved_into_transcript = 0 then interpreter 
+    else { interpreter with 
+        lines_added_since_last_input = moved_into_transcript;
+        transcript = transcript_with_pending;
+        pending_lines = [] } in
+
+    if new_interpreter.lines_added_since_last_input <= text_max_height then 
+        (* We can keep on partying *)
+        next_instruction new_interpreter
+    else 
+        (* We are over-full. Move some of the lines into pending. Move one more than we need to 
+        to make room for the --MORE-- line *)
+        
+        let (new_transcript, new_pending, moved_into_pending) = move new_interpreter.transcript [] (new_interpreter.lines_added_since_last_input - text_max_height + 1) in
+        { new_interpreter with 
+            transcript = new_transcript; 
+            pending_lines = new_pending; 
+            state = Printing_is_waiting_for_key;
+            lines_added_since_last_input = new_interpreter.lines_added_since_last_input - moved_into_pending
+        };;
+
+(* TODO: This assumes that there are no newlines in the string *)
+let do_print interpreter s = 
+    let (new_transcript, lines_added) = wrap_lines (add_to_lines interpreter.transcript s) text_max_width in
+    try_to_complete_printing { interpreter with 
+        transcript = new_transcript; 
+        lines_added_since_last_input = interpreter.lines_added_since_last_input + lines_added };;
+    
+let do_newline interpreter =
+    try_to_complete_printing { interpreter with 
+        transcript = "" :: interpreter.transcript; 
+        lines_added_since_last_input = interpreter.lines_added_since_last_input + 1 };;
+  
+let complete_prompting interpreter s =
+    let (new_transcript, _) = wrap_lines (add_to_lines interpreter.transcript s) text_max_width in 
+    next_instruction { interpreter with transcript = "" :: new_transcript ; lines_added_since_last_input = 0 };;
+   
+let step interpreter =
+    match (interpreter.state, interpreter.instructions) with
+    | (_, [])  
+    | (Halted, _) -> failwith "halted"
+    | (Printing_is_waiting_for_key, _) -> failwith "waiting for key" 
+    | (Prompting_is_waiting_for_string, _) -> failwith "waiting for string"
+    | (Running, (Print s) :: _) -> do_print interpreter s
+    | (Running, Newline :: _) -> do_newline interpreter  
+    | (Running, Prompt :: _) -> { interpreter with state = Prompting_is_waiting_for_string }
+    | (Running, Quit :: _ ) -> { interpreter with state = Halted };;
+    
+let step_with_input interpreter input =
+    match (interpreter.state, interpreter.instructions) with
+    | (Printing_is_waiting_for_key, _) -> try_to_complete_printing interpreter
+    | (Prompting_is_waiting_for_string, _) -> complete_prompting interpreter input
+    | _ -> failwith "not expecting input"
+    
+let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";;
+let prompt = "> ";;
+
+let main_interpreter = {
+    instructions = [ 
+    Print lorem;
+    Newline;
+    Print prompt;
+    Prompt;
+    Print lorem;
+    Print lorem;
+    Newline;
+    Print prompt;
+    Prompt;
+    Newline;
+    Print prompt;
+    Prompt;
+    Newline;
+    Print prompt;
+    Prompt
+        ];
+    state = Running; 
+    transcript = []; 
+    lines_added_since_last_input = 0;
+    pending_lines = []
+    };;
+    
+    
+let draw_transcript transcript = 
+    clear_graph();
+    display_lines transcript 100 100 text_max_height 10;;
+        
+let wait_for_key interpreter = 
+    draw_transcript ("--- MORE ---" :: interpreter.transcript) ;
+    let status = wait_next_event events in
+    let key = string_of_char status.key in
+    step_with_input interpreter key;;
+    
+let wait_for_string interpreter max =
     let rec input_loop buffer =
-        draw buffer;
+        let transcript = interpreter.transcript in
+        let (to_display, _) = wrap_lines (add_to_lines transcript buffer) text_max_width in
+        draw_transcript to_display;
+        
         let status = wait_next_event events in
         let key = string_of_char status.key in
         let length = String.length buffer in
-        if key = "x" then failwith "quitting"
-        else if key = "\r" then buffer
+        if key = "\r" then buffer
         else if key = "\b" then (
             if length = 0 then input_loop buffer 
             else input_loop (String.sub buffer 0 (length - 1)))
         else if length > max then input_loop buffer
         else input_loop (buffer ^ key) in
-    input_loop "";;
+    let result = input_loop "" in
+    step_with_input interpreter result;;
 
-let rec main_loop transcript =
-    let draw user_input =
-        clear_graph ();
-        let to_display = add_to_lines transcript user_input in
-        let wrapped = wrap_lines to_display 40 in
-        display_lines wrapped 100 100 200 10 in
-    let input = get_input draw 80 in
-    let result = "blah blah blah" in
-    let added_input = wrap_lines (add_to_lines transcript input) 40 in
-    let added_result = wrap_lines (result :: added_input) 40 in
-    let added_prompt = " >" :: added_result in
-    main_loop added_prompt;;    
-        
-main_loop [" >"];;
+let rec main_loop interpreter =
+    let new_interpreter = step interpreter in
+    draw_transcript new_interpreter.transcript;
+    match new_interpreter.state with
+    | Running -> main_loop new_interpreter
+    | Halted -> ()
+    | Printing_is_waiting_for_key -> main_loop (wait_for_key new_interpreter)
+    | Prompting_is_waiting_for_string -> main_loop (wait_for_string new_interpreter 80);;
+
+main_loop main_interpreter;;
