9e02d5a2ab8aae26375cb54cd127af1aa021a69f
ericlippert <eric@lippert.com>
2016-01-28 22:54:41 -0800

    Add minizork v3 demo

--- flathead.ml
+++ flathead.ml
@@ -1,5 +1,5 @@
 open Type;;
-let story = Story.load_story "Trinity.DAT" ;;
+let story = Story.load_story "MiniZork.Z3" ;;
 let screen = Screen.make (Character_height 50) (Character_width 80) ;;
 let interpreter = Interpreter.make story screen ;;
 let debugger = Debugger.make interpreter ;;
