11eeeda46d1fc7671c0abeba138bcb852c177eb3
ericlippert <eric@lippert.com>
2016-01-30 13:24:56 -0800

    Code cleanup in zstring processing

--- zstring.ml
+++ zstring.ml
@@ -35,12 +35,15 @@ strings, and is half the real address in v3 but different for other versions. *)
 let decode_word_address (Word_zstring word_address) =
   Zstring (word_address * 2)
 
+let first_abbrev_addr (Abbreviation_table_base base) =
+  Word_address base
+
 let abbreviation_address story (Abbreviation n) =
   if n < 0 || n >= abbreviation_table_length then
     failwith "bad offset into abbreviation table"
   else
-    let (Abbreviation_table_base base) = Story.abbreviations_table_base story in
-    let abbr_addr = Word_address (base + (n * 2)) in
+    let base = first_abbrev_addr (Story.abbreviations_table_base story) in
+    let abbr_addr = inc_word_addr_by base n in
     let word_addr = Word_zstring (Story.read_word story abbr_addr) in
     decode_word_address word_addr
 
@@ -100,18 +103,18 @@ let rec read story (Zstring address) =
 (* A debugging method for looking at memory broken up into the
 1 / 5 / 5 / 5 bit chunks used by zstrings. *)
 
-let display_bytes story offset length =
-  let rec aux i acc =
-    if i > length then acc
-    else (
-      let word = Story.read_word story (Word_address (offset + i)) in
-      let is_end = fetch_bits bit15 size1 word in
-      let zchar1 = fetch_bits bit14 size5 word in
-      let zchar2 = fetch_bits bit9 size5 word in
-      let zchar3 = fetch_bits bit4 size5 word in
-      let s = Printf.sprintf "%04x(%01x %02x %02x %02x) " word is_end zchar1 zchar2 zchar3 in
-      aux (i + 2) (acc ^ s)) in
-    aux 0 ""
+let display_bytes story addr =
+  let rec aux current acc =
+    let word = Story.read_word story current in
+    let is_end = fetch_bits bit15 size1 word in
+    let zchar1 = fetch_bits bit14 size5 word in
+    let zchar2 = fetch_bits bit9 size5 word in
+    let zchar3 = fetch_bits bit4 size5 word in
+    let s = Printf.sprintf "%02x %02x %02x " zchar1 zchar2 zchar3 in
+    let acc = acc ^ s in
+    if is_end = 1 then acc
+    else aux (inc_word_addr current) acc in
+  aux addr ""
 
 (* Debugging helper *)
 let display_abbreviation_table story =
