5e14867335f743411d870b7db5f38de597fc47db
ericlippert <eric@lippert.com>
2016-01-29 20:07:44 -0800

    Add some error checking to memory

--- immutable_bytes.ml
+++ immutable_bytes.ml
@@ -17,14 +17,24 @@ let make bytes =
   { original_bytes = bytes; edits = IntMap.empty }
 
 let read_byte bytes (Byte_address address) =
-  let c =
-    if IntMap.mem address bytes.edits then IntMap.find address bytes.edits
-    else bytes.original_bytes.[address] in
-  int_of_char c
+  if address < 0 then
+    failwith "address is negative"
+  else if address >= String.length bytes.original_bytes then
+    failwith "address is out of range"
+  else
+    let c =
+      if IntMap.mem address bytes.edits then IntMap.find address bytes.edits
+      else bytes.original_bytes.[address] in
+    int_of_char c
 
 let write_byte bytes (Byte_address address) value =
-  let b = char_of_int (byte_of_int value) in
-  { bytes with edits = IntMap.add address b bytes.edits }
+  if address < 0 then
+    failwith "address is negative"
+  else if address >= String.length bytes.original_bytes then
+    failwith "address is out of range"
+  else
+    let b = char_of_int (byte_of_int value) in
+    { bytes with edits = IntMap.add address b bytes.edits }
 
 let original bytes =
   { bytes with edits = IntMap.empty }
