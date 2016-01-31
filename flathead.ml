196832ddf91e42271e6848e371062df8a16fb843
ericlippert <eric@lippert.com>
2016-01-31 08:48:59 -0800

    Update driver to read long string

--- flathead.ml
+++ flathead.ml
@@ -3,9 +3,6 @@ open Utility
 
 let () = 
   let story = Story.load "minizork.z3" in
-  let zstring = Zstring.abbreviation_zstring story (Abbreviation 0) in
+  let zstring = Zstring 0xb106 in
   let text = Zstring.read story zstring in
   Printf.printf "%s\n" text;
-  let zstring = Zstring.abbreviation_zstring story (Abbreviation 1) in
-  let text = Zstring.read story zstring in
-  Printf.printf "%s\n" text
